a97c2c6900a9f8be381a21a2631eea6c39a634fa
PRAVEEN <105143196+praveenCSE1@users.noreply.github.com>
2023-12-28 09:33:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.post('/login', async (req, res) => {
 
     const user = await db.collection('users').findOne({ email, password });
     if (user) {
-      return res.status(200).json({ message: 'Login successful', redirect: '/' });
+      return res.status(200).json({ message: 'Login successful', redirect: '/signed' });
     } else {     
       return res.status(401).json({ message: 'Invalid credentials' });
     }
